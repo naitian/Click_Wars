7621ed85d96eb763b0cbc0ce0df8cb4631225044
naitian <nzhou2011@gmail.com>
2015-03-06 15:43:54 -0500

    So many things.
    Commented out a lot of console logs
    
    Added ready property
    
    Added game_on variable.
    
    Added refreshing player.

--- click_wars.js
+++ click_wars.js
@@ -1,36 +1,51 @@
-console.log("js called");
+//console.log("js called");
 var Hangout = gapi.hangout;
-console.log("Hangout: " + Hangout);
-console.log("api done");
+//console.log("Hangout: " + Hangout);
+//console.log("api done");
 var participants = new Array();
 var scores = new Array();
 //var participant_id = new Array();
 var avatar_list = document.getElementById("avatars");
 var names_list = document.getElementById("names");
+var body = document.getElementById("body");
+var game_on = false;
 console.log("vars made");
 
 function init(){
-    //document.getElementById("body").innerHTML += "hey guys. What's up?";
-    participants = Hangout.getEnabledParticipants();
-    console.log("Participants: " + participants);
-    for(var i = 0; i < participants.length; i++){
-        console.log(avatar_list);
-        //avatar_list.innerHTML += "innerhtml div test";
-        avatar_list.innerHTML += "<li><img src = '" + participants[i].person.image.url + "' class = 'avatar_pic' /> <br /><span class = 'name'>" + participants[i].person.displayName + "</span></li>";
-        
-
-        //participant_id[i] = participants[i].person.id;
-
-        //participant_id[i] = participants.id;
-        console.log("URL:" + i + " " + participants[i].person.image.url);
-        console.log("Display Name:" + i + " " + participants[i].person.displayName);
-        //console.log("Id:" + i + " " + participants_id[i]);
-
+    body.style.width = window.innerWidth;
+    body.style.height = window.innerHeight;
+    if(game_on){
+        body.innerHTML = "Too late, bruh. A game is already in session. You know what they say. You snooze, you lose. Loser.";
     }
-    
+    else {
+        //document.getElementById("body").innerHTML += "hey guys. What's up?";
+        participants = Hangout.getEnabledParticipants();
+
+        console.log("Participants: " + participants);
+        for(var i = 0; i < participants.length; i++){
+            //console.log(avatar_list);
+            participants[i].ready = false;
+            //avatar_list.innerHTML += "innerhtml div test";
+            avatar_list.innerHTML += "<li><img src = '" + participants[i].person.image.url + "' class = 'avatar_pic' /> <br /><span class = 'name'>" + participants[i].person.displayName + "</span></li>";
+
+
+            //participant_id[i] = participants[i].person.id;
+
+            //participant_id[i] = participants.id;
+            console.log("URL:" + i + " " + participants[i].person.image.url);
+            console.log("Display Name:" + i + " " + participants[i].person.displayName);
+            //console.log("Id:" + i + " " + participants_id[i]);
+
+        }
+    }
 }
 
 gapi.hangout.onApiReady.add(function(eventObj){
     console.log("api ready");
+    init();
+});
+
+gapi.hangout.onParticipantsChanged.add(function(eventObj){
+    console.log("changed people");
     init();
 });
