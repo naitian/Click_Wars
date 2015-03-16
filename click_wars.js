75bab184f64983fbb6b0abf445469095175fa906
naitian <nzhou2011@gmail.com>
2015-03-16 14:03:28 -0400

    separation anxiety
    separated init into init() and refresh()

--- click_wars.js
+++ click_wars.js
@@ -34,6 +34,35 @@ function score(player){
     localScores[participants.length] += 1;
 }
 
+function refresh(){
+    if(game_on){
+        body.innerHTML = "Too late, bruh. A game is already in session. You know what they say. You snooze, you lose. Loser.";
+    }
+    else {
+        //document.getElementById("body").innerHTML += "hey guys. What's up?";
+        participants = Hangout.getEnabledParticipants();
+        avatar_list.innerHTML = "";
+        ready_div.innerHTML = "";
+        console.log("Participants: " + participants);
+        for(var i = 0; i < participants.length; i++){
+            //console.log(avatar_list);
+            //avatar_list.innerHTML += "innerhtml div test";
+            avatar_list.innerHTML += "<li><img src = '" + participants[i].person.image.url + "' class = 'avatar_pic' id='a" + i + "' onclick='score(" + i +  ")'/> <br /><span class = 'name'>" + participants[i].person.displayName + "</span></li>";
+            document.getElementById("a" + i).style.borderColor = "#fff";
+            if(participants[i].ready === true) document.getElementById("a" + i).style.borderColor = "#2ecc71";
+
+            //participant_id[i] = participants[i].person.id;
+
+            //participant_id[i] = participants.id;
+            console.log("URL:" + i + " " + participants[i].person.image.url);
+            console.log("Display Name:" + i + " " + participants[i].person.displayName);
+            //console.log("Id:" + i + " " + participants_id[i]);
+        }
+        ready_div.innerHTML += "<div onclick='toggleReady()'>Ready</div>";
+        console.log("ready div made");
+    }
+}
+
 function init(){
     body.style.width = window.innerWidth;
     body.style.height = window.innerHeight;
@@ -48,26 +77,12 @@ function init(){
         console.log("Participants: " + participants);
         for(var i = 0; i < participants.length; i++){
             //console.log(avatar_list);
-            if(Hangout.data.getValue(participants[i].id) === 'undefined'){
-                participants[i].ready = false;
-                var ready_string = participants[i].ready.toString();
-                Hangout.data.setValue(participants[i].id, ready_string);
-                console.log(Hangout.data.getValue(participants[i].id));
-            }
-            localScores[i] = 0;
-            //avatar_list.innerHTML += "innerhtml div test";
-            avatar_list.innerHTML += "<li><img src = '" + participants[i].person.image.url + "' class = 'avatar_pic' id='a" + i + "' onclick='score(" + i +  ")'/> <br /><span class = 'name'>" + participants[i].person.displayName + "</span></li>";
-            document.getElementById("a" + i).style.borderColor = "#fff";
-            if(participants[i].ready === true) document.getElementById("a" + i).style.borderColor = "#2ecc71";
-
-
-            //participant_id[i] = participants[i].person.id;
-
-            //participant_id[i] = participants.id;
-            console.log("URL:" + i + " " + participants[i].person.image.url);
-            console.log("Display Name:" + i + " " + participants[i].person.displayName);
-            //console.log("Id:" + i + " " + participants_id[i]);
+            participants[i].ready = false;
+            var ready_string = participants[i].ready.toString();
+            Hangout.data.setValue(participants[i].id, ready_string);
+            console.log(Hangout.data.getValue(participants[i].id));        
         }
+        refresh();
         ready_div.innerHTML += "<div onclick='toggleReady()'>Ready</div>";
         console.log("ready div made");
     }
@@ -81,10 +96,10 @@ Hangout.onApiReady.add(function(eventObj){
 
 Hangout.onParticipantsChanged.add(function(eventObj){
     console.log("changed people");
-    init();
+    refresh();
 });
 
 Hangout.data.onStateChanged.add(function(eventObj){
     console.log("toggled ready");
-    init();
+    refresh();
 });
